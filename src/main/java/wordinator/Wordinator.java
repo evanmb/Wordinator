6649d2b9a6f3bcbf16be23889295cdd47eb43b7c
evan.blackwell@mchsi.com <evan.blackwell@mchsi.com>
2016-04-11 11:54:55 -0500

    Project Structure

---
+++ src/main/java/wordinator/Wordinator.java
@@ -0,0 +1,17 @@
+package wordinator;
+
+import org.parse4j.Parse;
+
+/**
+ * Hello world!
+ *
+ */
+public class Wordinator 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+		Parse.initialize("qUQ4H5VsD1t1fmQvJTZIvM76bPrEgNVR5sWAn9Vy",
+				"juVtcRqhhYCjVqFDngDM0KoQYxj1EpEAIPmFuOvA");
+    }
+}
